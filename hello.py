710ea654c6f84020386e12c11529808125f41ace
Derek Mok <derek.mok9201@gmail.com>
2016-02-10 00:40:48 +0000

    Removing some useless stuff

--- hello.py
+++
@@ -1,8 +0,0 @@
-# Demo file for Spyder Tutorial
-# Hans Fangohr, University of Southampton, UK
-
-def hello():
-    """Print "Hello World" and return None"""
-    print("Good and interesting World")
-
-# main program starts here
