97214402a474b80c5192e6eba9182d3896488e1f
Derek Mok <derek.mok9201@gmail.com>
2016-02-14 01:14:53 +0000

    Moved discount calculation code out of Algorithm_1.py

--- Algorithm_1.py
+++ Algorithm_1.py
@@ -1,152 +1,122 @@
-#Discount Algorithm 1: 
+# Discount Algorithm 1:
 
-#Key assumptions: the shop under consideration sells only one product and every customer always uses the app. 
+# Key assumptions: the shop under consideration sells only one product and every customer always uses the app.
 
-#Each transaction, you choose from four options (of equal expected gain): True Gambler (TG), Average Gambler (AG), Safe Player (SP) and Moral Person (MP) in the ascending order of winning probability (25%, 50%, 75% and 100%). 
+# Each transaction, you choose from four options (of equal expected gain): True Gambler (TG), Average Gambler (AG), Safe
+# Player (SP) and Moral Person (MP) in the ascending order of winning probability (25%, 50%, 75% and 100%).
 
-#Main principle: 
-#You risk big, you win/lose big.
-#For example, if you are a TG, not only do you save significantly more/less money when you win/lose but also your odds at the next transaction improves/drops more dramatically. On the other hand, if you're so ethical that you insist on option MP, you always save some money but not an impressive/dissapointing amount. And you odds remain the same. The odds are adjusted by varying the desired profit increase \alpha in a certain range. 
-#DS is testing
+# Main principle:
+# You risk big, you win/lose big.
+# For example, if you are a TG, not only do you save significantly more/less money when you win/lose but also your odds
+# at the next transaction improves/drops more dramatically. On the other hand, if you're so ethical that you insist on
+# option MP, you always save some money but not an impressive/disappointing amount. And you odds remain the same. The
+# odds are adjusted by varying the desired profit increase \alpha in a certain range.
+# DS is testing
 
-import numpy as np
 import random
+from discount import GamblerType, calculate_discount, coffee_price
 
-def DiscountDeals(alpha, R, Option,p,r):
-    ''' This function returns winning probability q_w and discount rates d_w and d_l when you win/lose. 
-    alpha :a positive real number, goal profit increase
-    R     :a positive real number, expected increase in the no of customers /day
-    Option:an integer, a choice of discount options, 1 = TG, 2 = AG, 3 = CG and 4 = MP. 
-    Output:[q_w,d_w,d_l] = [winning probability, discount rate when you win, discount rate when you lose]. 
-    '''
-    if Option == 1: #True Gambler
-        q_w = 0.25 #winning prob
-        grad_l = -(1-q_w)/q_w
-        intcept_l = (alpha/R + r/p*(1-alpha/R))/q_w
-        Start = max(r/p, q_w*intcept_l)
-        End = min(1,(r/p - intcept_l)/grad_l)
-        d_l = (1-q_w)*(End - Start) + Start
-        d_w = grad_l*d_l + intcept_l
-        return [q_w, d_w, d_l]
-    elif Option == 2: #Average Gambler
-        q_w = 0.5 #winning prob
-        grad_l = -(1-q_w)/q_w
-        intcept_l = (alpha/R + r/p*(1-alpha/R))/q_w
-        Start = max(r/p, q_w*intcept_l)
-        End = min(1,(r/p - intcept_l)/grad_l)
-        d_l = (1-q_w)*(End - Start) + Start
-        d_w = grad_l*d_l + intcept_l
-        return [q_w, d_w, d_l]
-    elif Option == 3: #Conservative Gambler
-        q_w = 0.75 #winning prob
-        grad_l = -(1-q_w)/q_w
-        intcept_l = (alpha/R + r/p*(1-alpha/R))/q_w
-        Start = max(r/p, q_w*intcept_l)
-        End = min(1,(r/p - intcept_l)/grad_l)
-        d_l = (1-q_w)*(End - Start) + Start
-        d_w = grad_l*d_l + intcept_l
-        return [q_w, d_w, d_l]
-    elif Option == 4: #Moral Person
-        q_w = 1
-        d_w = (alpha/R + r/p*(1-alpha/R))
-        d_l = (alpha/R + r/p*(1-alpha/R))
-        return [q_w, d_w, d_l]               
-        
-          
-def Algorithm_1(alpha=1.1,R=1.2,range_alpha=[1.0,1.2], no_inc = 10): 
-    '''
-    This is the main algorithm which starts an interactive simulation. 
-    alpha:the desired rate of profit increase (>1)
-    R    :the expected rate of increase in no. of customers. 
-    range_alpha : [lower bound, upper bound], range of alpha e.g. Here the upper bdd is strictly smaller than R to make sure that you always save money even when you lose!
-    no_inc      : no of default increments for each update of R
-    '''
+
+def command_line_client(alpha=1.1, rate_of_customer_increase=1.2, range_alpha=[1.0, 1.2], no_inc=10):
+    """
+    This is the main algorithm which starts an interactive simulation.
+    :param alpha:the desired rate of profit increase (>1)
+    :param rate_of_customer_increase    :the expected rate of increase in no. of customers.
+    :param range_alpha : [lower bound, upper bound], range of alpha e.g. Here the upper bdd is strictly smaller than R
+    to make sure that you always save money even when you lose!
+    :param no_inc      : no of default increments for each update of R
+    """
     print ""
     print "                     Welcome to the world of gambling!"
     print ""
-    #Fixed parameters:
-    p = 3.5 # price of a coffee
-    r = 0.8 # raw cost of a coffee 
-    print "The price for a coffee is %s $." % p
-    
-    #Fundamentals:
-    width_inc = (range_alpha[1] - range_alpha[0])/no_inc #default width of an increment
-    History = [] #history of past transactions
-    gambler_type = 4 #initial gambler type 
-    gambler_labels = ['True Gambler', 'Average Gambler', 'Safe Player', 'Moral Person']
+    print "The price for a coffee is %s $." % coffee_price
+
+    # Fundamentals:
+    width_inc = (range_alpha[1] - range_alpha[0]) / no_inc  # default width of an increment
+    history = []  # history of past transactions
     save = 0
     no_win = 0
     no_lose = 0
-    weight  = [1.0, 0.75, 0.5, 0] #when you win/lose, decrease/increase alpha by width_inc*weight[gambler_type-1].
-    Continue = 1 
-            
-    while Continue == 1:
-        
-        #Display 4 gambling options: 
+    should_continue = 1
+
+    while should_continue == 1:
+
+        # Display 4 gambling options:
         print '---------------------'
         print "Choose from below:"
         print " "
-        for Option in range(1,4):
-            [q_w, d_w, d_l] = DiscountDeals(alpha, R, Option,p,r)    
-            print "          %s : %s, Winning Probability = %s %%" %(Option,gambler_labels[Option-1],int(100*q_w))
-            #print "  Win and get %s    OR    Lose and get %s" %(d_w,d_l)
-            print "          Win and save %s $    OR    Lose and save %s $" %(round(p*(1-d_w),2),round(p*(1-d_l),2))
-            print " "
-        
-        [q_w, d_w, d_l] = DiscountDeals(alpha, R, 4,p,r)    
-        print "          %s : %s, Guaranteed save %s $" %(4,gambler_labels[3], round(p*(1-d_w),2))
+
+        # Print the output for immoral gamblers
+        for gambler_type in GamblerType:
+            if gambler_type != GamblerType.moral:
+                [q_w, d_w, d_l] = calculate_discount(alpha, rate_of_customer_increase, gambler_type)
+                print "          %s : %s, Winning Probability = %s %%" % (
+                    gambler_type.input_option, gambler_type.description, int(100 * q_w))
+                # print "  Win and get %s    OR    Lose and get %s" %(d_w,d_l)
+                print "          Win and save %s $    OR    Lose and save %s $" % (
+                    round(coffee_price * (1 - d_w), 2), round(coffee_price * (1 - d_l), 2))
+                print " "
+
+        # Print the output for our scrupulous friends out there (moral person)
+        [q_w, d_w, d_l] = calculate_discount(alpha, rate_of_customer_increase, GamblerType.moral)
+        print "          %s : %s, Guaranteed save %s $" % (GamblerType.moral.input_option,
+                                                           GamblerType.moral.description,
+                                                           round(coffee_price * (1 - d_w), 2))
         print " "
-        
-        #Ask for a prefeered gambling option:
-        gambler_type  = raw_input("Which type of gambler are you?: ")
-        
-        #Show the result of the gamble and update the odds:
-        while not(gambler_type in ['1','2','3','4']):
-            gambler_type  = raw_input("Please choose from options 1, 2, 3 or 4.")
-        gambler_type = int(gambler_type)    
-            
-        [q_w, d_w, d_l] = DiscountDeals(alpha, R, gambler_type, p,r) 
-        
-        dice = random.random() #Generate a random number [0,1)
+
+        # Ask for a preferred gambling option:
+        input_option = raw_input("Which type of gambler are you?: ")
+
+        # Show the result of the gamble and update the odds:
+        while not (input_option in ['1', '2', '3', '4']):
+            input_option = raw_input("Please choose from options 1, 2, 3 or 4.")
+        input_option = int(input_option)
+
+        gambler_type = GamblerType.lookup_gambler_by_input(input_option)
+        [q_w, d_w, d_l] = calculate_discount(alpha, rate_of_customer_increase,
+                                             gambler_type)
+
+        dice = random.random()  # Generate a random number [0,1)
         if dice < q_w:
-            win = round(p*(1-d_w),2)
+            win = round(coffee_price * (1 - d_w), 2)
             print "Congratulations! You won %s $!" % win
-            save   += p*(1-d_w)
-            no_win +=1 
-            History += ['W',[q_w, d_w, d_l]] #update history
-            
-            if (alpha - width_inc*weight[gambler_type-1]) > range_alpha[0]:
-                alpha -= width_inc*weight[gambler_type-1] # update alpha!
+            save += coffee_price * (1 - d_w)
+            no_win += 1
+            history += ['W', [q_w, d_w, d_l]]  # update history
+
+            if (alpha - width_inc * gambler_type.probability) > range_alpha[0]:
+                alpha -= width_inc * gambler_type.probability  # update alpha!
             else:
                 alpha = range_alpha[0]
         else:
-            lose = round(p*(1-d_l),2)
+            lose = round(coffee_price * (1 - d_l), 2)
             print "Sorry, you only won on %s $!" % lose
-            save   += p*(1-d_l)
-            no_lose +=1 
-            History += ['L',[q_w, d_w, d_l]] #update history
-            
-            if (alpha + width_inc*weight[gambler_type-1]) < range_alpha[1]:
-                alpha += width_inc*weight[gambler_type-1] # update alpha!
+            save += coffee_price * (1 - d_l)
+            no_lose += 1
+            history += ['L', [q_w, d_w, d_l]]  # update history
+
+            if (alpha + width_inc * gambler_type.probability) < range_alpha[1]:
+                alpha += width_inc * gambler_type.probability  # update alpha!
             else:
                 alpha = range_alpha[1]
-        print "You've saved %s $ so far!" % round(save,2)   
-        
-        #Ask whether to cotinue:
-        Continue  = raw_input("Would you like to continue (1 = yes, 0 = no): ")     
-        while not(Continue == '0' or Continue == '1'):
-            Continue  = raw_input("Please press number 1 or 0 (1 = yes, 0 = no): ")
-        Continue = int(Continue)
-      
+        print "You've saved %s $ so far!" % round(save, 2)
+
+        # Ask whether to cotinue:
+        should_continue = raw_input("Would you like to continue (1 = yes, 0 = no): ")
+        while not (should_continue == '0' or should_continue == '1'):
+            should_continue = raw_input("Please press number 1 or 0 (1 = yes, 0 = no): ")
+        should_continue = int(should_continue)
+
     print "---------------------"
     print "Thanks for playing the game :)"
     print "Summary:"
     print "You've saved:", save
-    
+
     no_total = no_win + no_lose
-    print "You've won %s times out of %s games" %(no_win, no_total)
+    print "You've won %s times out of %s games" % (no_win, no_total)
     print "Check variable 'History' to see the history of your past transactions."
-    return History
-    
+    return history
+
+
 # Run an interactive session:    
-History = Algorithm_1(alpha=1.1,R=1.35,range_alpha=[1.0,1.35], no_inc = 10)
+command_line_client(alpha=1.1, rate_of_customer_increase=1.35, range_alpha=[1.0, 1.35], no_inc=10)
---
+++ discount.py
@@ -0,0 +1,44 @@
+from enum import Enum
+
+coffee_price = 3.5  # price of a coffee
+coffee_cost = 0.8  # raw cost of a coffee
+
+
+class GamblerType(Enum):
+    __order__ = 'true average conservative moral'
+    true = (0.25, 1, "True Gambler")
+    average = (0.5, 2, "Average Gambler")
+    conservative = (0.75, 3, "Conservative Gambler")
+    moral = (1, 4, "Moral Person")
+
+    def __init__(self, probability, input_option, description):
+        self.probability = probability
+        self.description = description
+        self.input_option = input_option
+
+    @classmethod
+    def lookup_gambler_by_input(cls, input_option):
+        for gambler in cls:
+            if gambler.input_option == input_option:
+                return gambler
+
+
+def calculate_discount(alpha, rate_of_customer_increase, gambler_type):
+    """ This function returns winning probability q_w and discount rates d_w and d_l when you win/lose.
+    :return: Output:[q_w,d_w,d_l] = [winning probability, discount rate when you win, discount rate when you lose].
+    :param gambler_type: an integer, a choice of discount options, 1 = TG, 2 = AG, 3 = CG and 4 = MP.
+    :param rate_of_customer_increase: a positive real number, expected increase in the no of customers /day
+    :param alpha: a positive real number, goal profit increase
+    """
+    return _calculate_discount_given_probability(alpha, gambler_type.probability, rate_of_customer_increase)
+
+
+def _calculate_discount_given_probability(alpha, q_w, rate_of_customer_increase):
+    grad_l = -(1 - q_w) / q_w
+    intercept_l = (alpha / rate_of_customer_increase + coffee_cost / coffee_price * (
+        1 - alpha / rate_of_customer_increase)) / q_w
+    start = max(coffee_cost / coffee_price, q_w * intercept_l)
+    end = min(1, (coffee_cost / coffee_price - intercept_l) / grad_l) if grad_l != 0 else 1
+    d_l = (1 - q_w) * (end - start) + start
+    d_w = grad_l * d_l + intercept_l
+    return [q_w, d_w, d_l]
